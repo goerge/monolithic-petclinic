97af4c79cf549e3d511f76b49950b8f17d68b384
markus.claessens <markus.claessens@ebcont.com>
2020-07-30 14:17:10 +0200

    Refactoring.

--- monolith/src/main/java/org/springframework/samples/petclinic/vets/VetService.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/vets/VetService.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.vets;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -20,9 +19,10 @@ public class VetService {
 	}
 
 	public Collection<VetDto> allVets() {
-		List<VetDto> vets = vetsRepository.findAll().stream().map(v -> new VetDto(v.getId(), v.getFirstName(), v.getLastName(),
-				v.getSpecialties().stream().map(Specialty::getName).collect(Collectors.toSet()))).collect(Collectors.toList());
+		return vetsRepository.findAll().stream().map(this::getVetDto).collect(Collectors.toList());
+	}
 
-		return vets;
+	private VetDto getVetDto(Vet v) {
+		return new VetDto(v.getId(), v.getFirstName(), v.getLastName(), v.getSpecialties().stream().map(Specialty::getName).collect(Collectors.toSet()));
 	}
 }
